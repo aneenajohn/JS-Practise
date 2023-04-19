7d01d88eaa9cc6995cd54d9c1ad89aa39d94954d
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2023-04-19 10:03:30 +0530

    Added var vs let vs const
    Signed-off-by: aneenajohn <75213145+aneenajohn@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # JS Practise
  This repo is for practising JS
+
+
+# VAR Vs Let Vs Const
+
+## Scoping:
+  - Var has function scope 
+  - Let and Const have block scope.
+
+## Redeclaration:
+ - Var can be redeclared 
+ - let and const can be redeclared
+
+## Redefinition:
+ - Var and let can be redefined
+ - Const cant be redefined
+
---
+++ var-let-const.js
@@ -0,0 +1,37 @@
+var aa = 20;
+{
+    var a = 10;
+}
+
+console.log("Var a, aa: ", a, aa);
+
+let bb = 30;
+{
+    let b = 20;
+}
+
+const cc = 100;
+{
+    const c = 30
+}
+// console.log("const c: ", c); // NOTE:  ReferenceError: c is not defined
+// console.log("let b: ", b , bb); // NOTE:  ReferenceError: b is not defined
+
+// DOCS: Var has function scope whereas let and const have block scioping hence not available outside the block
+
+
+var a =50;
+
+console.log("Redeclared var a: ", a);
+
+// let  bb = 303;
+
+// console.log("Redeclared let bb: ", bb); //NOTE: SyntaxError: Identifier 'bb' has already been declared
+
+// DOCS: Var can be redeclared, let and const can be redeclared
+
+a= 40;
+bb = 5050;
+cc = 1000; // NOTE: TypeError: Assignment to constant variable.
+
+console.log("a,bb,cc", a,bb,cc);
