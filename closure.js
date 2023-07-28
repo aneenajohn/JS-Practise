fc24cbf3e53fd1a022292b11690e50acfc174773
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2023-07-28 10:52:51 +0530

    A non-closure method added
    Signed-off-by: aneenajohn <75213145+aneenajohn@users.noreply.github.com>

--- closure.js
+++ closure.js
@@ -1,3 +1,5 @@
+// DOCS: Version 1
+
 var counter = (function () {
     var myCounter = 0;
     function changeBy(val) {
@@ -18,13 +20,47 @@ var counter = (function () {
     };
   })();
 
+console.log(counter.value());
+// debugger;
+counter.increment();
+counter.increment();
+console.log(counter.value());
+counter.decrement();
+console.log(counter.value());
+counter.increment();
+console.log(counter.value());
 
-  console.log(counter.value());
+
+// DOCS: Version 2 - Not a closure
+
+function counter2() {
+  var myCounter = 0;
+  function changeBy(val) {
+    console.log("Before Val: ", val, myCounter)
+    myCounter += val;
+    console.log("After Val: ", val, myCounter)
+  }
+
+  return {
+    increment: function() {
+      changeBy(1)
+    },
+    decrement: function() {
+      changeBy(-1)
+    },
+    value: function() {
+      return myCounter;
+    }
+  }
+}
+
+
+  console.log(counter2().value());
   // debugger;
-  counter.increment();
-  counter.increment();
-  console.log(counter.value());
-  counter.decrement();
-  console.log(counter.value());
-  counter.increment();
-  console.log(counter.value());
+  counter2().increment();
+  counter2().increment();
+  console.log(counter2().value());
+  counter2().decrement();
+  console.log(counter2().value());
+  counter2().increment();
+  console.log(counter2().value());
