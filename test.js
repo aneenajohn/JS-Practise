758e63689d5dd7268807d317f3b7aa0518738f8f
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-09-06 17:55:31 +0530

    Added sample test for closure applications
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

---
+++ test.js
@@ -0,0 +1,59 @@
+var counterFunc = (function () {
+    let count = 0;
+    function changeBy(val) {
+      count += val;
+    }
+  
+    return {
+      increment: function() {
+        changeBy(1)
+      },
+      decrement: function() {
+        changeBy(-1)
+      },
+      value: function() {
+        return count
+      }
+    }
+  })()
+  
+  console.log(counterFunc.value());
+  console.log(counterFunc.increment());
+  console.log(counterFunc.increment());
+  console.log(counterFunc.increment());
+  console.log(counterFunc.value());
+  console.log(counterFunc.decrement());
+  console.log(counterFunc.decrement());
+  console.log(counterFunc.value());
+
+
+  function testCount() {
+    let count = 0;
+
+    function changeBy(val){
+        count += val;
+        return count;
+    }
+
+    return {
+        increment: function() {
+            changeBy(1)
+        },
+        decrement: function() {
+            changeBy(-1)
+        },
+        value: function() {
+            return count;
+        }
+    }
+  }
+
+  const counter = testCount();
+  console.log(counter.value());
+  console.log(counter.increment());
+  console.log(counter.increment());
+  console.log(counter.increment());
+  console.log(counter.value());
+  console.log(counter.decrement());
+  console.log(counter.decrement());
+  console.log(counter.value());
