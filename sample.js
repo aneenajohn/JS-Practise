f1cb971852414a11b8d6536d75a533339c07de9f
aneenajohn <aneenajohn@gmail.com>
2023-10-26 20:08:15 +0530

    Updated comment
    Signed-off-by: aneenajohn <aneenajohn@gmail.com>

--- sample.js
+++ sample.js
@@ -8,7 +8,7 @@ const myObj = {
 
 const myObjStr = JSON.stringify(myObj);
 
-console.log("JSON.stringify Output:",myObjStr) // {“a”:”1”,”b”:2,”c”:{“d”:3,”e”:4}}
+console.log("JSON.stringify Output:",myObjStr) // {“a”:”1”,”b”:2,”c”:{“d”:3,”e”:4},"d":[1,2,3,4],"e":[{"a":"hello","b":123}]}
 
 
 function myStringifyFn(myObj){
@@ -19,7 +19,7 @@ function myStringifyFn(myObj){
     }else if(typeof myObj === 'object') {
         const stringifiedObj = Object.keys(myObj).map((key) => {
             const value = myStringifyFn(myObj[key]);
-            return '"' + key+ '"'+":"+ value
+            return '"' + key + '"' + ":" + value
         })
         return '{' + stringifiedObj.join(",") + '}';
     }else if(typeof myObj === 'string') {
@@ -30,4 +30,3 @@ function myStringifyFn(myObj){
 }
 const myObjStr2 = myStringifyFn(myObj);
 console.log("myStringifyFn output: ", myObjStr2) // {“a”:”1”,”b”:2,”c”:{“d”:3,”e”:4}}
-
