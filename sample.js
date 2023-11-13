047def9d8ab0f3eba5b030f50649258803a81850
aneenajohn <aneenajohn@gmail.com>
2023-11-13 16:59:58 +0530

    Added throttling for mouseMove event handler
    Signed-off-by: aneenajohn <aneenajohn@gmail.com>

--- sample.js
+++ sample.js
@@ -3,11 +3,30 @@ const container = document.querySelector('.container');
 const pointer = document.querySelector('.pointer');
 
 let count =0;
-container.addEventListener('mousemove', (e) => {
+
+function onMouseMove (e) {
     const { offsetX, offsetY } =  e;
     const x = offsetX - 20;
     const y = offsetY + 20;
     pointer.style.transform = `translate(${x}px,${y}px)`;
     count++;
     console.log("Count: ", count);
-})
+}
+
+// container.addEventListener('mousemove', onMouseMove);
+
+
+const throttle = (func,delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if(now - prev > delay) {
+            prev = now;
+            return func(...args)
+        }
+    }
+}
+
+const throttledMouseMove =  throttle(onMouseMove, 100);
+
+container.addEventListener('mousemove', throttledMouseMove);
