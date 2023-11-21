5d4c7080e9934c8d8756727cbdd5a2d0f8c7ee76
aneenajohn <aneenajohn@gmail.com>
2023-11-21 22:13:59 +0530

    Better version of custom stringify added
    Signed-off-by: aneenajohn <aneenajohn@gmail.com>

--- sample.js
+++ sample.js
@@ -30,3 +30,43 @@ function myStringifyFn(myObj){
 }
 const myObjStr2 = myStringifyFn(myObj);
 console.log("myStringifyFn output: ", myObjStr2) // {“a”:”1”,”b”:2,”c”:{“d”:3,”e”:4}}
+
+
+// {“a”:”1”,”b”:2,”c”:{“d”:3,”e”:4},"d":[1,2,3,4],"e":[{"a":"hello","b":123}]}
+function myStringify(obj) {
+    if(Array.isArray(obj)) {
+        const arrStr = obj.map((item) => myStringify(item)).join(",");
+        return `[${arrStr}]`
+    }else if(typeof obj === "string"){
+        return `"${obj}"`
+    }else if(typeof obj === "number"){
+        return obj;
+    }else if(typeof obj === "object" && obj !== null) {
+        const keys = Object.keys(obj);
+        const objStr = keys.map((key) => `"${key}":${myStringify(obj[key])}`).join(",");
+        return `{${objStr}}`
+    }else {
+        return "null";
+    }
+}
+
+
+console.log(myStringify(myObj))
+
+let obj2 = {
+    "a": 20,
+    "b": "aa",
+    "c": [1,2,3,4],
+    "d": {
+        "aa": 12,
+        "bb": "abcde",
+        "cc": [11,12,13,14],
+        "dd": {
+            "aaa": 1,
+            "bbb": "asdf"
+        }
+    }
+}
+
+console.log("JSON Response 2: ", JSON.stringify(obj2))
+console.log("Custom JSON:",myStringify(obj2))
