b0abbf124a8f50cfe91918836175329fecf7b99f
Aneena John <aneenajohn@gmail.com>
2024-07-31 16:29:23 +0530

    Add object based fix for new key added on mac with define Prop
    Signed-off-by: Aneena John <aneenajohn@gmail.com>

---
+++ nightmares/Objects/obj1.js
@@ -0,0 +1,48 @@
+const myObj = {
+  dessert: 'Tres Leches',
+  drink: 'Two and half men',
+  starter: 'tippi tippi',
+};
+
+myObj.test = 'Blub';
+
+Object.defineProperty(myObj, 'test2', {
+  value: 'Blubbbb',
+});
+
+// console.log('From for each:');
+// Object.keys(myObj).forEach((key) => {
+//   console.log(key);
+// });
+
+// console.log('From map:');
+// Object.keys(myObj).map((key) => console.log(key));
+
+console.log('From for in loop:');
+for (key in myObj) {
+  console.log(key);
+}
+
+console.log('Printed all the properties of myObj');
+
+const a = [1, 2, 3];
+a.myFunction = () => {
+  console.log('hello');
+};
+
+Object.defineProperty(a, 'myFunc2', {
+  value: () => {
+    console.log('myFunc2 below');
+  },
+  enumerable: true,
+  configurable: true,
+  writable: true,
+});
+
+Object.keys(a).forEach((key) => {
+  console.log(key);
+});
+
+console.log('Printed all the properties of array object');
+
+a.myFunc2();
